9fa3e2584dac786537b9ddca2c002792e50a818e
Wrik Das <wrikdas22@gmail.com>
2023-02-09 16:29:00 +0530

    added eureka service

---
+++ eureka/src/test/java/com/dwrik/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dwrik.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
