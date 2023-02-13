1089b30b12656e7aa3917c168e8d55c0f46662b6
Wrik Das <wrikdas22@gmail.com>
2023-02-13 11:31:24 +0530

    created user repository

--- auth/src/main/java/com/dwrik/auth/repository/UserRepository.java
+++ auth/src/main/java/com/dwrik/auth/repository/UserRepository.java
@@ -4,6 +4,9 @@ import com.dwrik.auth.model.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
+	Optional<User> findByEmail(String email);
 }
