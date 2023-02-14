e055917184817863174b013f497defe9d39136e9
Wrik Das <wrikdas22@gmail.com>
2023-02-14 16:47:09 +0530

    added handler for handling exceptions occured during updation of seats

--- flight/src/main/java/com/dwrik/flight/exception/FlightControllerAdvisor.java
+++ flight/src/main/java/com/dwrik/flight/exception/FlightControllerAdvisor.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import javax.validation.ConstraintViolationException;
+import java.sql.SQLException;
 import java.util.Map;
 
 @RestControllerAdvice
@@ -12,10 +14,19 @@ public class FlightControllerAdvisor {
 
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	@ExceptionHandler(IllegalArgumentException.class)
-	public Map<String, Object> handleInvalidDateExceptions(IllegalArgumentException e) {
+	public Map<String, Object> handleInvalidDateException(IllegalArgumentException e) {
 		return Map.of(
 				"status", HttpStatus.BAD_REQUEST.value(),
 				"error", "invalid date"
+		);
+	}
+
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler({UnknownFlightException.class, ConstraintViolationException.class, SQLException.class})
+	public Map<String, Object> handleUpdateException(RuntimeException e) {
+		return Map.of(
+				"status", "failed",
+				"error", "invalid flight or unable to reserve seat"
 		);
 	}
 }
