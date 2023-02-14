73442e240938bd0add32add398208b1985aa63cf
Wrik Das <wrikdas22@gmail.com>
2023-02-14 22:34:06 +0530

    added consumer for booking cancellation

---
+++ flight/src/main/java/com/dwrik/flight/config/FlightConfig.java
@@ -0,0 +1,22 @@
+package com.dwrik.flight.config;
+
+import com.dwrik.flight.service.FlightService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.function.Consumer;
+
+@Configuration
+public class FlightConfig {
+
+	@Autowired
+	private FlightService flightService;
+
+	@Bean
+	public Consumer<Long> onFlightIdReceive() {
+		return (id) -> {
+			flightService.vacateSeat(id);
+		};
+	}
+}
