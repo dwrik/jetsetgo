820bb6be3bc04e87e91e50a9c485eb223bc56241
Wrik Das <wrikdas22@gmail.com>
2023-02-14 11:22:35 +0530

    refactored flight controller to use service interfaces

--- flight/src/main/java/com/dwrik/flight/controller/FlightController.java
+++ flight/src/main/java/com/dwrik/flight/controller/FlightController.java
@@ -4,8 +4,10 @@ import com.dwrik.flight.model.Flight;
 import com.dwrik.flight.service.FlightService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
@@ -17,17 +19,18 @@ public class FlightController {
 	private FlightService flightService;
 
 	@GetMapping("/")
+	@ResponseStatus(HttpStatus.ACCEPTED)
 	public Iterable<Flight> getAll() {
 		return flightService.getAllFlights();
 	}
 
 	@GetMapping("/{from}/{to}/{date}")
+	@ResponseStatus(HttpStatus.ACCEPTED)
 	public Iterable<Flight> getUsingFromToAndDate(
 			@PathVariable(value = "from") String source,
 			@PathVariable(value = "to") String destination,
 			@PathVariable @DateTimeFormat(pattern = "dd-MM-yyyy") Date date
 	) {
-		System.out.println(source + ", " + destination + ", " + date);
 		return flightService.getFlightsUsingSourceAndDestinationAndDate(source, destination, date);
 	}
 }
