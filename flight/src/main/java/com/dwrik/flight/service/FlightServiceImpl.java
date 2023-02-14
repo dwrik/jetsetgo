66885e9477905fb3226f62f042755f04fe517175
Wrik Das <wrikdas22@gmail.com>
2023-02-14 16:47:47 +0530

    added method for reserving seats

--- flight/src/main/java/com/dwrik/flight/service/FlightServiceImpl.java
+++ flight/src/main/java/com/dwrik/flight/service/FlightServiceImpl.java
@@ -1,11 +1,14 @@
 package com.dwrik.flight.service;
 
+import com.dwrik.flight.exception.UnknownFlightException;
 import com.dwrik.flight.model.Flight;
 import com.dwrik.flight.repository.FlightRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.Date;
+import java.util.Optional;
 
 @Service
 public class FlightServiceImpl implements FlightService {
@@ -19,5 +22,21 @@ public class FlightServiceImpl implements FlightService {
 
 	public Iterable<Flight> getFlightsUsingSourceAndDestinationAndDate(String source, String destination, Date date) {
 		return flightRepository.findBySourceAndDestinationAndDate(source, destination, date);
+	}
+
+	@Transactional
+	public Integer reserveSeat(Long id) {
+		Optional<Flight> result = flightRepository.findById(id);
+
+		if (result.isEmpty()) {
+			throw new UnknownFlightException("no such flight exists");
+		}
+
+		Flight flight = result.get();
+		int seatNumber = flight.getRemainingSeats();
+		flight.setRemainingSeats(seatNumber - 1);
+		flightRepository.save(flight);
+
+		return seatNumber;
 	}
 }
