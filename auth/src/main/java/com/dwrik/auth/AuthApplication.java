cf31aa01f6eeb9cf97f61d425a020c381b617b1b
Wrik Das <wrikdas22@gmail.com>
2023-02-15 15:51:40 +0530

    implemented command line runner for preloading user data

--- auth/src/main/java/com/dwrik/auth/AuthApplication.java
+++ auth/src/main/java/com/dwrik/auth/AuthApplication.java
@@ -1,13 +1,36 @@
 package com.dwrik.auth;
 
+import com.dwrik.auth.model.User;
+import com.dwrik.auth.repository.UserRepository;
+import com.dwrik.auth.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class AuthApplication {
+public class AuthApplication implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+	@Value("${ADMIN_EMAIL}")
+	private String adminEmail;
+
+	@Value("${ADMIN_PASSWORD}")
+	private String adminPassword;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) {
+		userRepository.save(new User(1, adminEmail, passwordEncoder.encode(adminPassword)));
+	}
 }
