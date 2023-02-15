71a93dcc4d097581724fadb821e944e24824d669
Wrik Das <wrikdas22@gmail.com>
2023-02-15 12:59:28 +0530

    removed redundant comments

--- gateway/src/main/java/com/dwrik/gateway/filter/AuthFilter.java
+++ gateway/src/main/java/com/dwrik/gateway/filter/AuthFilter.java
@@ -18,14 +18,12 @@ import java.util.function.Predicate;
 @Component
 public class AuthFilter implements GatewayFilter {
 
-	// list of secured endpoints
 	private final List<String> securedEndpoints = List.of(
 			"/reserve",
 			"/booking",
 			"/checkin"
 	);
 
-	// predicate for validating routes
 	private final Predicate<ServerHttpRequest> isSecuredEndpoint =
 			request -> securedEndpoints.stream().anyMatch(path -> request.getURI().getPath().contains(path));
 
@@ -34,29 +32,22 @@ public class AuthFilter implements GatewayFilter {
 
 	@Override
 	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-		// get request
 		ServerHttpRequest request = exchange.getRequest();
 
-		// check if secured endpoint
 		if (isSecuredEndpoint.test(request)) {
-			// check if auth header missing
 			if (isAuthHeaderMissing(request)) {
 				return onError(exchange, HttpStatus.UNAUTHORIZED);
 			}
 
-			// get auth token from header
 			final String token = getAuthToken(request);
 
-			// check if auth token valid
 			if (jwtUtil.isTokenInvalid(token)) {
 				return onError(exchange, HttpStatus.BAD_REQUEST);
 			}
 
-			// extract claims from token and add to headers
 			populateRequestWithHeaders(exchange, token);
 		}
 
-		// forward request to next filter in chain
 		return chain.filter(exchange);
 	}
 
