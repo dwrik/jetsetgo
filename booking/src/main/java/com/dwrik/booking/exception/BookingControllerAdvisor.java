a523ee63939eb5caf03272cee96b89df94d06868
Wrik Das <wrikdas22@gmail.com>
2023-02-16 15:56:44 +0530

    added handler for FeignException

--- booking/src/main/java/com/dwrik/booking/exception/BookingControllerAdvisor.java
+++ booking/src/main/java/com/dwrik/booking/exception/BookingControllerAdvisor.java
@@ -1,5 +1,6 @@
 package com.dwrik.booking.exception;
 
+import feign.FeignException;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -14,6 +15,16 @@ import java.util.Map;
 public class BookingControllerAdvisor {
 
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public Map<String, Object> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+		Map<String, String> map = new HashMap<>();
+		for (FieldError error : e.getBindingResult().getFieldErrors()) {
+			map.put(error.getField(), error.getDefaultMessage());
+		}
+		return Map.of("error", map);
+	}
+
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	@ExceptionHandler({UnknownBookingException.class, UnableToCreateBookingException.class})
 	public Map<String, Object> handleUnknownBookingAndUnableToCreateBookingException(RuntimeException e) {
 		return Map.of(
@@ -23,12 +34,11 @@ public class BookingControllerAdvisor {
 	}
 
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public Map<String, Object> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-		Map<String, String> map = new HashMap<>();
-		for (FieldError error : e.getBindingResult().getFieldErrors()) {
-			map.put(error.getField(), error.getDefaultMessage());
-		}
-		return Map.of("error", map);
+	@ExceptionHandler(FeignException.class)
+	public Map<String, Object> handleFeignException(FeignException e) {
+		return Map.of(
+				"status", HttpStatus.BAD_REQUEST.value(),
+				"error", "failed to get seat confirmation"
+		);
 	}
 }
