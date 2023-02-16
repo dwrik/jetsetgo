cde3ea72348d9e3619ef044ebdfc7ad2f760ef4f
Wrik Das <wrikdas22@gmail.com>
2023-02-16 15:57:16 +0530

    added checkin service

---
+++ checkin/src/main/java/com/dwrik/checkin/CheckinApplication.java
@@ -0,0 +1,13 @@
+package com.dwrik.checkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckinApplication.class, args);
+	}
+
+}
---
+++ checkin/src/test/java/com/dwrik/checkin/CheckinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dwrik.checkin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
