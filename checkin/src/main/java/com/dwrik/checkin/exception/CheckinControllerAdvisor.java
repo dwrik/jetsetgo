1cdf53c72b7e682dd3df675172de75eb758a181e
Wrik Das <wrikdas22@gmail.com>
2023-02-17 16:00:07 +0530

    added custom exception and controller advisor

---
+++ checkin/src/main/java/com/dwrik/checkin/exception/CheckinControllerAdvisor.java
@@ -0,0 +1,32 @@
+package com.dwrik.checkin.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.validation.ConstraintViolationException;
+import java.sql.SQLException;
+import java.util.Map;
+
+@RestControllerAdvice
+public class CheckinControllerAdvisor {
+
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(UnableToCheckinException.class)
+	public Map<String, Object> handleUnableToCheckinException(UnableToCheckinException e) {
+		return Map.of(
+				"status", HttpStatus.BAD_REQUEST.value(),
+				"error", e.getMessage()
+		);
+	}
+
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	@ExceptionHandler({ConstraintViolationException.class, SQLException.class})
+	public Map<String, Object> handleCheckinCreationException(RuntimeException e) {
+		return Map.of(
+				"status", HttpStatus.INTERNAL_SERVER_ERROR.value(),
+				"error", "something went wrong"
+		);
+	}
+}
---
+++ checkin/src/main/java/com/dwrik/checkin/exception/UnableToCheckinException.java
@@ -0,0 +1,8 @@
+package com.dwrik.checkin.exception;
+
+public class UnableToCheckinException extends RuntimeException {
+
+	public UnableToCheckinException(String message) {
+		super(message);
+	}
+}
